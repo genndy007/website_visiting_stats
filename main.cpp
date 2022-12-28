5e954aacbe3778e1baefbc3efd44e1b3b7088903
genndy007 <kochevgenazp@gmail.com>
2022-12-28 18:14:11 +0200

    Add ip address counting

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
-#include <iostream>
+#include <vector>
 #include <map>
+#include <iostream>
 
 // Own code
 #include "helper.h"
@@ -8,9 +9,41 @@
 
 using namespace std;
 
+// sort by most frequent ip addresses
+struct IpToAmount{
+    string ip;
+    int amount;
+
+    string toString() {
+        return "IP: " + ip + "; times: " + to_string(amount);
+    }
+};
+
+vector<IpToAmount> getSortedIpToAmount(vector<VisitStat> stats) {
+    map<string, int> ipToAmount;
+    string ip;
+    for (auto stat : stats) {
+        ip = stat.visitorIp;
+        if (!ipToAmount.count(ip)) ipToAmount[ip] = 1;
+        else ipToAmount[ip]++;      
+    }
+
+    vector<IpToAmount> ipToAmtList;
+    for (auto kv : ipToAmount) {
+        IpToAmount ipToAmt = {kv.first, kv.second};
+        ipToAmtList.push_back(ipToAmt);
+    }
+
+    for (auto iptoa : ipToAmtList) {
+        cout << iptoa.toString() << endl;
+    }
+
+    return ipToAmtList;
+}
+
 // MAIN PROGRAM LOOP
 int main() {
     vector<VisitStat> stats = readVisitStatsFromFileName("data/stats.txt");
-    printBrowserByPopularity(stats);
+    getSortedIpToAmount(stats);
     return 0;
 }
