2030040468047900e1177a59d4c1e3cbdc5e1bc1
genndy007 <kochevgenazp@gmail.com>
2022-12-28 21:48:02 +0200

    Train to parse website name from url

--- main.cpp
+++ main.cpp
@@ -1,4 +1,4 @@
-
+#include <iostream>
 
 // Own code
 #include "helper.h"
@@ -8,10 +8,43 @@
 
 using namespace std;
 
+// resource referral frequency
+
+string getResourceNameFromUrl(string url) {
+    // calculate offset from beginning
+    int offset = 0;
+    vector<string> protocols = {"http://", "https://"};
+    for (auto protocol : protocols) {
+        int prefixLength = protocol.length();
+        // check if prefix is equal
+        if (url.compare(0, prefixLength, protocol) == 0){
+            offset = prefixLength;
+            break;
+        }
+    }
+    // take from url between http(s):// and next /
+    string resourceName = "";
+    for (int i = offset; i < url.length(); i++) {
+        char ch = url[i];
+        if (ch == '/') break;
+        resourceName += ch;
+    }
+
+    return resourceName;
+}
+
+void getSortedReferrals(vector<VisitStat> stats) {
+    for (auto stat : stats) {
+        cout << getResourceNameFromUrl(stat.referral) << endl;
+    }
+}
+
 
 // MAIN PROGRAM LOOP
 int main() {
     vector<VisitStat> stats = readVisitStatsFromFileName("data/stats.txt");
-    printTopNMostFreqIp(stats, 3);
+    // getSortedReferrals(stats);
+    // getResourceNameFromUrl("https://ololo.ua/popit/lookfor");
+    getSortedReferrals(stats);
     return 0;
 }
